9112039d829f4d9e4a2e2103d85f22ff78f6a3ca
NITRO_OG <137004224+Fearlessff@users.noreply.github.com>
2025-05-21 17:48:06 +0100

    Delete nitro_og_site directory

--- nitro_og_site/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const logo = document.querySelector('.logo');
-  if (logo) {
-    logo.addEventListener('click', () => {
-      alert('Nitro OG - Powering up on Solana!');
-    });
-  }
-});
